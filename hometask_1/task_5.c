a00508e38597152335a1917ffa20823c00719e29
dimantik <sluhdimaa@gmail.com>
2019-12-16 01:58:37 +0300

    Commit

---
+++ hometask_1/task_5.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void shiftByOneIndex(int *array, int length);
+
+int main() {
+    int m, n, length;
+
+    printf("Input m: ");
+    scanf("%d", &m);
+    printf("Input n: ");
+    scanf("%d", &n);
+
+    length = m + n;
+    int array[length];
+
+    for (int i = 0; i < length; i++) {
+        printf("array[%d} = ", i);
+        scanf("%d", &array[i]);
+    }
+
+    for (int i = 0; i < length; i++) {
+        printf("\narray[%d} = %d", i, array[i]);
+    }
+
+    printf("\nReverse");
+
+    for (int i = 0; i < n; i++) {
+        shiftByOneIndex(array, length);
+    }
+
+    for (int i = 0; i < length; i++) {
+        printf("\narray[%d} = %d", i, array[i]);
+    }
+
+    return 1;
+}
+
+void shiftByOneIndex(int *array, int length) {
+
+    int helpVal, previousElement;
+
+    for (int i = 0; i < length; i++) {
+
+        helpVal = *(array + i);
+
+        if (i == 0) {
+            previousElement = helpVal;
+            continue;
+        }
+
+        *(array + i) = previousElement;
+        previousElement = helpVal;
+        
+        if (i == length - 1) {
+            *(array) = previousElement;
+        }
+    }
+
+}
