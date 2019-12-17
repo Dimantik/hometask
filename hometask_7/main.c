8a3e8be5f2749fa6ceb4d83bf0978e109980987b
dimantik <sluhdimaa@gmail.com>
2019-12-17 19:27:39 +0300

    Commit

--- hometask_7/main.c
+++ hometask_7/main.c
@@ -1,32 +1,33 @@
+  
 #include <stdio.h>
 
-#define LENGTH 5
-
-void input();
-void other();
-
-int main() {
-	input();
-	return 0;
-}
-
 void other() {
-	printf("\n***Function other was worked out.***\n");
+
+	printf("\n --- BUFFER OVERFLOW! --- \n\n");
+
 }
 
 void input() {
 
-	int  data[LENGTH], i = 0, x = 0;
+	int data[3], i = 0, x = 0;
 
-	printf("Addresses: data[3] = %x, data[4] = %x, data[5] = %x, other = %p\n", data[3], data[4], data[5], &other);
-	
+	printf("Adr: %x %x %x\n", &data[3], &data[4], &data[5]);
+
+	printf("Adr of other: %p\n", &other);
+
 	scanf("%x", &x);
-
-	while (x != 0) {
+	while (x) {
 
 		data[i] = x;
-		i++;
 		scanf("%x", &x);
+		i++;
 
 	}
+}
+
+int main() {
+
+	input();
+	return 0;
+
 }
