59f96950978bda98fe9da8ed9423083aa21c09e1
dimantik <sluhdimaa@gmail.com>
2019-12-25 03:13:28 +0300

    Commit

--- hometask_5/main.c
+++ hometask_5/main.c
@@ -8,6 +8,14 @@ void testFunction();
 int main () {
 
     int *dynamicPointer = (int*)malloc(sizeof(int));
+
+    if (dynamicPointer == NULL) {
+
+        printf("Memmory was not allocated.");
+        return 0;
+
+    }
+
     *dynamicPointer = 6;
 
     int localVariable = 10;
@@ -18,11 +26,11 @@ int main () {
     userFunctionPointer = testFunction;
     systemFunctionPointer = printf;
 
-    printf("\nAdress local variable = %x", &localVariable);        //6422288
-    printf("\nAdress global variable = %x", &globalVariable);      //4223088
-    printf("\nAdress dynamic variable = %x", dynamicPointer);      //11211856
-    printf("\nAdress user function = %x", userFunctionPointer);    //4199611
-    printf("\nAdress sytem function = %x", systemFunctionPointer); //4209440
+    printf("\nAdress local variable = %x", &localVariable);        
+    printf("\nAdress global variable = %x", &globalVariable);     
+    printf("\nAdress dynamic variable = %x", dynamicPointer);      
+    printf("\nAdress user function = %x", userFunctionPointer);    
+    printf("\nAdress sytem function = %x", systemFunctionPointer); 
 
     return 1;
 }
